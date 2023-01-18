91e12c6b53bc6425198c49bc519dfa2ff0ce97e1
jiwon <jiwon5568@nate.com>
2023-01-18 18:48:26 +0900

    env setting modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { BoardsModule } from './boards/boards.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
-    UsersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      //connectionName: 'default',
     }),
+    UsersModule,
     AuthModule,
+    BoardsModule,
+    CommentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 
-
-
-
 export class AppModule {}
+
