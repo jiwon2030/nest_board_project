bde919d12fcc719386b9a310bd39284ed7344d88
jiwon <jiwon5568@nate.com>
2023-01-30 18:19:47 +0900

    닉네임, 비밀번호 DTO 수정

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -23,17 +23,30 @@ export class FindLoginUserDTO extends PickType(User, [
     'nickname'
 ] as const) {}
 
-export class UserNicknameChangeDTO extends PickType(User, ['id'] as const) {
+export class testFindLoginUserDTO extends PickType(User, [
+    '_id',
+    'nickname'
+] as const) {
+    id: string;
+}
+
+export class UserNicknameChangeDTO extends PickType(User, ['_id'] as const) {
     @ApiProperty({
         example: 'testnickname',
     })
     nickname: string;
 }
 
-export class UserPwdChangeDTO extends PickType(User, ['id'] as const) {
+export class UserPwdChangeDTO extends PickType(User, ['_id'] as const) {
     @ApiProperty({
       example: '1234qwer',
     })
+    id: string;
     password: string;
     salt: string;
+}
+
+export class Testuserpwddto{
+    id: string;
+    password: string;
 }
